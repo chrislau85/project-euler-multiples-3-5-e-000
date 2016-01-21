cab7f41ab0a3a3fa01236b69544739943cbea7aa
chrislau85 <chrislau85@gmail.com>
2016-01-21 01:21:18 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,34 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples = []
+  x = 0
+  x3 = 0
+  x5 = 0
+
+  limit.times do
+    x += 1
+    x3 = x * 3
+    x5 = x * 5
+    if x3 < limit
+      multiples.push(x3)
+    end
+    if x5 < limit
+       multiples.push(x5)
+    end
+  end
+  return multiples.uniq.sort
+end
+
+collect_multiples(10)
+collect_multiples(100)
+collect_multiples(1000)
+
+def sum_multiples(limit)
+  arr =  collect_multiples(limit)
+  total = 0
+  arr.each{|number| total += number}
+  return total
+end
+
+sum_multiples(10)
+sum_multiples(100)
+sum_multiples(1000)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,39 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  # constructor method
+  def initialize(limit)
+    @limit = limit
+    sum_multiples
+  end
+
+  def collect_multiples
+    multiples = []
+    x = 0
+    x3 = 0
+    x5 = 0
+
+    @limit.times do
+      x += 1
+      x3 = x * 3
+      x5 = x * 5
+      if x3 < @limit
+        multiples.push(x3)
+      end
+      if x5 < @limit
+         multiples.push(x5)
+      end
+    end
+    return multiples.uniq.sort
+  end
+
+  def sum_multiples
+    arr =  collect_multiples
+    total = 0
+    arr.each{|number| total += number}
+    return total
+  end
+end
+
+multiples = Multiples.new(10)
+multiples = Multiples.new(100)
+multiples = Multiples.new(1000)
